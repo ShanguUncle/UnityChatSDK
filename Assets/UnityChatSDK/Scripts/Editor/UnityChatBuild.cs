b2c859aabdde5bb928665d797d9aaf6172d49feb
SG <1785670525@qq.com>
2020-11-16 16:16:11 +0800

    v6.3update

---
+++ Assets/UnityChatSDK/Scripts/Editor/UnityChatBuild.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+#if UNITY_IOS
+using UnityEditor.iOS.Xcode;
+#endif
+
+public class UnityChatBuild 
+{
+#if UNITY_IOS
+    [PostProcessBuild(1)]
+    public static void OnPostProcessBuild(BuildTarget target, string path)
+    {
+
+        if (target == BuildTarget.iOS)
+        {
+            PBXProject project = new PBXProject();
+            string filePath = PBXProject.GetPBXProjectPath(path);
+            project.ReadFromFile(filePath);
+
+            //GetUnityMainTargetGuid()用于app获取Guid,GetUnityFrameworkTargetGuid()用于插件获取Guid
+            string guid = project.GetUnityFrameworkTargetGuid();
+#if !UNITY_2019_3_OR_NEWER
+      guid = project.TargetGuidByName(project.GetUnityTargetName());
+#endif
+            project.AddBuildProperty(guid, "OTHER_LDFLAGS", "-lturbojpeg");
+            File.WriteAllText(filePath, project.WriteToString());
+        }
+    }
+#endif
+}
